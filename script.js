da1043be76a39d3819af12ad5856a979cc3f4008
Julia <julia_ahlen@hotmail.com>
2026-08-19 11:31:12 +0200

    scriptindex

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-const name = "Anna";
-console.log(`Hello ${name}!`);
-console.log(`Ett fint namn: ${name}!`);
+const inputText = document.getElementById('inputText');
+const addBtn = document.getElementById('add');
+const deleteBtn = document.getElementById('delete');
+const myList = document.getElementById('todoList');
+
+let selectedTask = null;
+
+addBtn.addEventListener('click', function() {
+    const textValue = inputText.value.trim();
+
+    if (textValue === '') {
+        return;
+    }
+
+    const listItem = document.createElement('li');
+    listItem.textContent = textValue;
+
+    listItem.addEventListener('click', function() {
+        selectedTask = listItem;
+
+        listItem.style.textDecoration =
+            listItem.style.textDecoration === 'line-through'
+                ? 'none'
+                : 'line-through';
+    });
+
+    myList.appendChild(listItem);
+    inputText.value = '';
+});
+
+deleteBtn.addEventListener('click', function() {
+    if (selectedTask !== null) {
+        selectedTask.remove();
+        selectedTask = null;
+    }
+});
